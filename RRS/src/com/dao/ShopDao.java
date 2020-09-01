e5f569a65d475809934e9fb3ac1108f2fbed4be9
Jinu <39368262+ruldy99@users.noreply.github.com>
2020-09-01 13:02:33 +0900

    Update ShopDao.java

--- RRS/src/com/dao/ShopDao.java
+++ RRS/src/com/dao/ShopDao.java
@@ -6,7 +6,7 @@ import com.frame.Dao;
 import com.vo.ShopVO;
 
 @Repository("sdao")
-
+adsf
 
 public interface ShopDao extends Dao<Integer, ShopVO> {
 	public void setcnt(Integer k) throws Exception;
